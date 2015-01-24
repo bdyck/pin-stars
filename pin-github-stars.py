abd1d437c7c90893f6ed4097eae7acaf21f7610b
Bryan Dyck <bryan@bjd.io>
2015-01-24 12:09:24 -0800

    Forgot to update copyright

--- pin-github-stars.py
+++ pin-github-stars.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2014 Bryan Dyck
+# Copyright (c) 2014-2015 Bryan Dyck
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
